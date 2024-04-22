aeeda68a499d2a751ce38c423922f1d3e9acc0e0
sahan <sahangawesh19@gmail.com>
2024-04-22 15:58:16 +0530

    mongodb atlas integration

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 3360;
 
-export const mongodbURL= "mongodb://127.0.0.1/RestaurantDBwAuth";
+export const mongodbURL="mongodb+srv://sahangawesh19:BzA0Q6KMTTRRqqYm@cluster0.tp7yvor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; //"mongodb://127.0.0.1/RestaurantDBwAuth";cdsd
